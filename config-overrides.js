eb41fc2db4613378ccf6124808c0318eea53c7cc
Juzo <fqqwsj@gmail.com>
2022-08-12 01:59:41 +0300

    header+routes fix

--- config-overrides.js
+++ config-overrides.js
@@ -10,6 +10,7 @@ module.exports = function override(config, env) {
     '@styles': 'src/styles',
     '@utils': 'src/utils',
     '@routes': 'src/routes',
+    '@static': 'src/static',
   })(config);
 
   return config;
--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,26 +1,27 @@
 import routesConfig from "@routes/routesConfig";
 import { NavLink, Route, Routes, BrowserRouter } from "react-router-dom";
-
+import Header from "@components/Header";
 import styles from './App.module.css';
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-        < NavLink to="/">Home</NavLink >
-        < NavLink to="/people">People</NavLink >
+        <div className={styles.wrapper}>
+        <Header />
         <Routes>
-          {routesConfig.map((route, index) => {
+          {routesConfig.map((route, index) => (
             <Route
               key={index}
               path={route.path}
               element={route.element}
             />
-          })}
-        </Routes>
+          ))}
+          </Routes>
+        </div>
       </BrowserRouter>
     </>
   )
 }
 
-export default App;                                                            
+export default App;                                                                 
